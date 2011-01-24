26c5e48fd41dbc001e7ae6c6cfe2f384b2bfda83
Gavin Miller <gavingmiller@gmail.com>
2011-01-23 19:41:37 -0700

    Changed the organization to match the implementation grouping

--- Classes/TwoByTwoCellView.h
+++ Classes/TwoByTwoCellView.h
@@ -10,26 +10,29 @@
 #import <QuartzCore/QuartzCore.h>
 
 @interface TwoByTwoCellView : UITableViewCell {
-	UILabel *topLeftValue;
-	UILabel *topRightValue;
-	UILabel *bottomLeftValue;
-	UILabel *bottomRightValue;
-	
 	UILabel *topLeftLabel;
+	UILabel *topLeftValue;
+	
 	UILabel *topRightLabel;
+	UILabel *topRightValue;
+	
 	UILabel *bottomLeftLabel;
+	UILabel *bottomLeftValue;
+	
 	UILabel *bottomRightLabel;
+	UILabel *bottomRightValue;
 }
 
-@property (nonatomic, retain) UILabel *topLeftValue;
-@property (nonatomic, retain) UILabel *topRightValue;
-@property (nonatomic, retain) UILabel *bottomLeftValue;
-@property (nonatomic, retain) UILabel *bottomRightValue;
-
 @property (nonatomic, retain) UILabel *topLeftLabel;
-@property (nonatomic, retain) UILabel *topRightLabel;
-@property (nonatomic, retain) UILabel *bottomLeftLabel;
-@property (nonatomic, retain) UILabel *bottomRightLabel;
+@property (nonatomic, retain) UILabel *topLeftValue;
 
+@property (nonatomic, retain) UILabel *topRightLabel;
+@property (nonatomic, retain) UILabel *topRightValue;
+
+@property (nonatomic, retain) UILabel *bottomLeftLabel;
+@property (nonatomic, retain) UILabel *bottomLeftValue;
+
+@property (nonatomic, retain) UILabel *bottomRightLabel;
+@property (nonatomic, retain) UILabel *bottomRightValue;
 
 @end
